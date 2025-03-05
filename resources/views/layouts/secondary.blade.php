86c45d52e848299e9e75a5cc0f10784bb89bb357
DaiDiT <darryl.zaidra@gmail.com>
2025-03-06 02:11:02 +0700

    add icon

--- resources/views/layouts/secondary.blade.php
+++ resources/views/layouts/secondary.blade.php
@@ -3,13 +3,13 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-	<link rel="icon" type="image/png" href="{{ asset('assets/img/icon.png') }}">
+	<link rel="icon" type="image/png" href="{{ asset('assets/img/logo.png') }}">
 	<title>SITA | {{ $title }}</title>
 	<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <script src="{{ asset('assets/js/script.js') }}"></script>
 </head>
-<body class="flex items-center justify-center h-screen bg-gray-100">
+<body class="flex items-center justify-center">
 	@yield('container')
 </body>
 </html>
