8b41b8139dcd93e98cf57b9b19ffef7d91f4b399
DaiDiT <darryl.zaidra@gmail.com>
2025-03-05 15:24:20 +0700

    Edit UI

--- resources/views/dosen/detail.blade.php
+++ resources/views/dosen/detail.blade.php
@@ -2,13 +2,45 @@
 
 @section('container')
 <div class="main-content">
-    <div class="flex flex-col items-center justify-center h-screen">
+    <div class="flex flex-col items-center justify-center">
         <h2 class="mb-4 text-3xl font-bold">{{ $mahasiswa->name }}</h2>
-        
-        <p>Judul TA: {{ $mahasiswa->tugasAkhir->judul ?? '-' }}</p>
 
         <!-- Tabel Detail Bimbingan -->
         <table class="table-auto border-collapse border border-gray-400 mt-4">
+            @if (session('error'))
+            <div id="alertBox" class="alert-box error">
+                <span>{{ session('error') }}</span>
+                <button id="closeAlert">✖</button>
+            </div>
+            @endif
+            @if (session('success'))
+            <div id="alertBox" class="alert-box success">
+                <span>{{ session('success') }}</span>
+                <button id="closeAlert">✖</button>
+            </div>
+            @endif
+            <div class="info-container">
+                Judul TA: <b><i>{{ $mahasiswa->tugasAkhir->judul ?? '-' }}</i></b>
+                
+                @php
+                    $persetujuanTA = optional($mahasiswa->tugasAkhir)->persetujuans->first();
+                @endphp
+
+                <div class="status-container">
+                    <b class="{{ $persetujuanTA->status === 'disetujui' ? 'text-green-600' : 'text-red-600' }}">
+                        Status: {{ $persetujuanTA->status }}
+                    </b>
+                    @if ($mahasiswa->bimbingans->count() > -1 && $persetujuanTA->status === 'diajukan')
+                    <form action="{{ route('persetujuan.ta') }}" method="POST">
+                        @method('PUT')
+                        @csrf
+                        <input name="type" value="tugas_akhir" hidden>
+                        <input name="tugas_akhir" value="{{ $mahasiswa->tugasAkhir->id }}" hidden>
+                        <button type="submit" name="status" value="disetujui" class="btn btn-green">Setujui Tugas Akhir</button>
+                    </form>
+                    @endif
+                <div>
+            </div>
             <thead>
                 <tr class="bg-gray-200">
                     <th class="border border-gray-400 px-4 py-2">No</th>
@@ -37,8 +69,8 @@
                                     @csrf
                                     <input name="type" value="bimbingan" hidden>
                                     <input name="bimbingan" value="{{ $bimbingan->id }}" hidden>
-                                    <button type="submit" name="status" value="disetujui" class="btn btn-green">✅ Setujui</button>
-                                    <button type="submit" name="status" value="ditolak" class="btn btn-red">❌ Tolak</button>
+                                    <button type="submit" name="status" value="disetujui" class="btn btn-green">Setujui</button>
+                                    <button type="submit" name="status" value="ditolak" class="btn btn-red">Tolak</button>
                                 </form>
                             @endif
                         </td>
@@ -52,23 +84,6 @@
                 @endforelse
             </tbody>
         </table>
-
-        @php
-            $persetujuanTA = optional($mahasiswa->tugasAkhir)->persetujuans->first();
-        @endphp
-        
-        @if ($mahasiswa->bimbingans->count() > 7 && $persetujuanTA->status === 'diajukan')
-            <form action="{{ route('persetujuan.ta') }}" method="POST">
-                @method('PUT')
-                @csrf
-                <input name="type" value="tugas_akhir" hidden>
-                <input name="tugas_akhir" value="{{ $mahasiswa->tugasAkhir->id }}" hidden>
-                <button type="submit" name="status" value="disetujui" class="btn btn-green">✅ Setujui</button>
-            </form>
-        @endif
-            <b class="{{ $persetujuanTA->status === 'disetujui' ? 'text-green-600' : 'text-red-600' }}">
-                Status: {{ $persetujuanTA->status }}
-            </b>
     </div>
 </div>
 @endsection
