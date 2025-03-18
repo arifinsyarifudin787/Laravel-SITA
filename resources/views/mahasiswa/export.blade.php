0bc28f1005a3f8831c50d56d7d9487e8b4a38d8f
DaiDiT <darryl.zaidra@gmail.com>
2025-03-19 00:06:16 +0700

    change view

--- resources/views/mahasiswa/export.blade.php
+++ resources/views/mahasiswa/export.blade.php
@@ -9,10 +9,6 @@
         td { padding: 3px;}
         th { text-align: center; font-weight: bold}
         th, .border td { border: 1px solid black;}
-        .border-x-0 {
-            border-left: white;
-            border-right: white;
-        }
         image {
             margin: 0;
             padding: 0;
@@ -20,43 +16,49 @@
     </style>
 </head>
 <body>
-    <table class="border" style="text-align: left; font-size: 16px">
+    <table class="border" style="text-align: left; font-size: 12px">
         <tr>
-            <td rowspan="4">
-                <img src="{{ asset('assets/img/logo_uin.png') }}" alt="" height="90px">
+            <td rowspan="4" style="padding:0">
+                @php
+                    $path = public_path('assets/img/logo_uin.png');
+                    $type = pathinfo($path, PATHINFO_EXTENSION);
+                    $data = file_get_contents($path);
+                    $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);    
+                @endphp
+                <img src="{{ $base64 }}" alt="" height="75px" style="margin-left: 5px">
             </td>
-            <td rowspan="4" style="font-size: 28px; text-align:center;"><b>FORM (FR)<b></td>
+            <td rowspan="4" style="font-size: 18px; text-align:center;"><b>FORM (FR)<b></td>
             <td>No. Dok.</td>
-            <td class="border-x-0">:</td>
+            <td>:</td>
             <td>FST-TU-AKM-FR-F.12</td>
         </tr>
         <tr>
             <td>Tgl. Terbit</td>
-            <td class="border-x-0">:</td>
+            <td>:</td>
             <td>1 September 2014</td>
         </tr>
         <tr>
             <td>No. Revisi</td>
-            <td class="border-x-0">:</td>
+            <td>:</td>
             <td>00</td>
         </tr>
         <tr>
             <td>Hal</td>
-            <td class="border-x-0">:</td>
+            <td>:</td>
             <td>1/1</td>
         </tr>
         <tr>
-            <td colspan="5" style="font-size: 24px; text-align:center;"><b>LEMBAR BIMBINGAN SKRIPSI</b></td>
+            <td colspan="5" style="font-size: 16px; text-align:center;"><b>LEMBAR BIMBINGAN SKRIPSI</b></td>
         </tr>
     </table>
-    <table style="width: 92%; margin-left: auto; margin-right: auto; font-size: 18px; margin-bottom: 20px">
+    <table style="width: 92%; margin-left: auto; margin-right: auto; font-size: 13px; margin-bottom: 40px">
         <tr>
             <td>NIM</td>
             <td>:</td>
-            <td>{{ $tugas_akhir->nim }}&emsp;&emsp;&emsp;&emsp;&emsp;</td>
-            <td>Tanggal SK</td>
+            <td style="white-space: pre;">{{ $tugas_akhir->nim }}                                    </td>
+            <td style="max-width: 110">Tanggal SK</td>
             <td>:</td>
-            <td></td>
+            <td style="white-space: pre;">{{ $tugas_akhir->created_at->translatedFormat('j F Y') }}                           </td>
         </tr>
         <tr>
             <td>NAMA</td>
@@ -67,12 +69,12 @@
             <td>{{ $pembimbing1 }}</td>
         </tr>
         <tr>
-            <td>Jurusan&emsp;&emsp;&emsp;</td>
+            <td style="white-space: pre;">Jurusan            </td>
             <td>:</td>
             <td>Teknik Informatika</td>
             <td>Pembimbing II</td>
             <td>:</td>
-            <td>{{ $pembimbing2 }}</td>
+            <td>{{$pembimbing2 }}</td>
         </tr>   
         <tr style="vertical-align: top">
             <td>Judul</td>
@@ -80,13 +82,13 @@
             <td colspan="4">{{ $tugas_akhir->judul }}</td>
         </tr>
     </table>
-    <table class="border" style="font-size: 18px;">
+    <table class="border" style="font-size: 13px;">
         <tr>
-            <th style="width: 60px">Perte- muan- ke</th>
-            <th style="width: 110px">Tanggal Bimbingan</th>
-            <th style="min-width: 300px">Materi Bimbingan</th>
-            <th style="min-width: 300px">Saran/ Perbaikan</th>
-            <th style="width: 120px">Paraf Pembimbing</th>
+            <th style="width: 50px">Perte- muan- ke</th>
+            <th style="width: 90px">Tanggal Bimbingan</th>
+            <th style="min-width: 200px">Materi Bimbingan</th>
+            <th style="min-width: 200px">Saran/ Perbaikan</th>
+            <th style="width: 100px">Paraf Pembimbing</th>
         </tr>
         @foreach ($bimbingan as $b)
         <tr style="vertical-align: top">
@@ -101,20 +103,27 @@
             <td></td>
         </tr>
         @endforeach
+        @for ($i = $bimbingan->count(); $i < 10; $i++)
+            <tr>
+                <td style="text-align: center; vertical-align: middle">{{ $i+1 }}</td>
+                <td style="white-space: pre;"> <br> </td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        @endfor
     </table>
-    <table style="margin-top: auto; margin-left: 10px; width: 90%; font-size: 18px;">
+    <table style="margin-top: 20px; margin-left: 10px; width: 90%; font-size: 13px;">
         <tr>
             <td>
                 Mengetahui,<br>
-                Ketua Jurusan<br><br><br><br>
-                <b>
-                    Dr. Dian Sa'adillah Maylawati, M.T.&emsp;&emsp;&emsp;
-                </b><br>
+                Ketua Jurusan<br><br><br><br><br><br>
+                <b style="white-space: pre;">Dr. Dian Sa'adillah Maylawati, M.T.              </b><br>
                 NIP. 198905262019032023
             </td>
             <td>
                 Bandung, {{ now()->translatedFormat('j F Y') }}<br>
-                Mahasiswa<br><br><br><br>
+                Mahasiswa<br><br><br><br><br><br>
                 <b>
                     {{ $mahasiswa->name }}
                 </b><br>
